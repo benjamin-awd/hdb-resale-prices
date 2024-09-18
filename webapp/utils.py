bfa56234266050735e092df38ba853c45c520cb7
Benjamin Dornel <benjamindornel@gmail.com>
2024-09-19 01:22:27 +0800

    fix: place data folder in webapp namespace

--- webapp/utils.py
+++ webapp/utils.py
@@ -2,4 +2,8 @@ from pathlib import Path
 
 
 def get_project_root() -> Path:
+    cloud_path = Path("/mount/src/hdb_resale_prices")
+
+    if cloud_path.exists():
+        return cloud_path
     return Path(__file__).parent.parent

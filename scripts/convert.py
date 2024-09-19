471f6873a0653a02791df88a7a0771c1faa0211a
Benjamin Dornel <benjamindornel@gmail.com>
2024-09-19 23:34:15 +0800

    refactor: read from parquet instead of csv

---
+++ scripts/convert.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+import polars as pl
+
+from webapp.utils import get_project_root
+
+
+def convert_lease(x):
+    if 0 < x <= 60:
+        result = "0-60 years"
+    elif 60 < x <= 80:
+        result = "61-80 years"
+    elif 80 < x <= 99:
+        result = "81-99 years"
+    return result
+
+
+def csv_to_parquet() -> pl.DataFrame:
+    """Combine all CSV files in the specified directory into a single parquet file"""
+    data_dir: Path = get_project_root() / "data"
+
+    schema = {
+        "_id": pl.Int64,
+        "month": pl.Utf8,
+        "town": pl.Utf8,
+        "flat_type": pl.Utf8,
+        "block": pl.Utf8,
+        "street_name": pl.Utf8,
+        "storey_range": pl.Utf8,
+        "floor_area_sqm": pl.Float32,
+        "flat_model": pl.Utf8,
+        "lease_commence_date": pl.Int16,
+        "remaining_lease": pl.Utf8,
+        "resale_price": pl.Float32,
+        "address": pl.Utf8,
+        "postal": pl.Int32,
+        "latitude": pl.Float32,
+        "longitude": pl.Float32,
+    }
+
+    df = pl.read_csv(data_dir / "*.csv", schema=schema, null_values="NIL")
+
+    df = df.with_columns(
+        (
+            pl.col("remaining_lease")
+            .str.extract(r"(\d+)", 1)
+            .cast(pl.Int64)
+            .alias("remaining_lease_years")
+        )
+    )
+
+    df = df.with_columns(
+        pl.col("remaining_lease_years")
+        .map_elements(convert_lease, pl.String)
+        .alias("cat_remaining_lease_years")
+    )
+
+    df = df.unique()
+    df = df.sort(by="month")
+    df.write_parquet(data_dir / "df.parquet")
+    return
+
+
+if __name__ == "__main__":
+    csv_to_parquet()
--- webapp/read.py
+++ webapp/read.py
@@ -21,39 +21,5 @@ def get_dataframe() -> pl.DataFrame:
     """Combine all CSV files in the specified directory into a single DataFrame."""
     data_dir: Path = get_project_root() / "data"
 
-    schema = {
-        "_id": pl.Int64,
-        "month": pl.Utf8,
-        "town": pl.Utf8,
-        "flat_type": pl.Utf8,
-        "block": pl.Utf8,
-        "street_name": pl.Utf8,
-        "storey_range": pl.Utf8,
-        "floor_area_sqm": pl.Float64,
-        "flat_model": pl.Utf8,
-        "lease_commence_date": pl.Int64,
-        "remaining_lease": pl.Utf8,
-        "resale_price": pl.Float64,
-        "address": pl.Utf8,
-        "postal": pl.Int64,
-        "latitude": pl.Float64,
-        "longitude": pl.Float64,
-    }
-
-    df = pl.read_csv(data_dir / "*.csv", schema=schema, null_values="NIL")
-    df = df.with_columns(
-        (
-            pl.col("remaining_lease")
-            .str.extract(r"(\d+)", 1)
-            .cast(pl.Int64)
-            .alias("remaining_lease_years")
-        )
-    )
-
-    df = df.with_columns(
-        pl.col("remaining_lease_years")
-        .map_elements(convert_lease, pl.String)
-        .alias("cat_remaining_lease_years")
-    )
-
-    return df.unique()
+    df = pl.read_parquet(data_dir / "*.parquet")
+    return df

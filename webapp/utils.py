2007125688c42a708237cbe6d44f6494f9bd995c
Benjamin Dornel <benjamindornel@gmail.com>
2024-09-21 14:43:51 +0800

    fix: cloud path

--- webapp/utils.py
+++ webapp/utils.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 
 def get_project_root() -> Path:
-    cloud_path = Path("/mount/src/hdb_resale_prices")
+    cloud_path = Path("/mount/src/hdb-kaki")
 
     if cloud_path.exists():
         return cloud_path

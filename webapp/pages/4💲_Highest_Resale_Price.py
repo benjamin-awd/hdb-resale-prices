548b4ef45176f6e2b49f1476cbf00008397104ba
Benjamin Dornel <benjamindornel@gmail.com>
2024-10-04 19:13:35 +0800

    refactor: add sidebar filter for highest resale price

--- webapp/pages/4💲_Highest_Resale_Price.py
+++ webapp/pages/4💲_Highest_Resale_Price.py
@@ -4,7 +4,7 @@ import polars as pl
 import streamlit as st
 from streamlit_folium import st_folium
 
-from webapp.read import load_dataframe
+from webapp.filter import SidebarFilter
 
 st.set_page_config(layout="wide")
 
@@ -16,33 +16,27 @@ st.write(
     "The colour of the pins reflect whether the unit is below or above the median value. Red indicates above median while green indicates below median"
 )
 
-# read data
-df = load_dataframe()
+sf = SidebarFilter(select_towns=(True, "multi"))
+
 # get highest price flat by town
-highest_price = df.filter(
+highest_price = sf.df.filter(
     pl.col("resale_price") == pl.col("resale_price").max().over(["town", "flat_type"])
 )
 
-# flat type data
-option_flat = st.selectbox(
-    "Select a flat type",
-    ("2 ROOM", "3 ROOM", "4 ROOM", "5 ROOM", "EXECUTIVE", "MULTI-GENERATION"),
-)
-
-highest_price_filtered = highest_price.filter(pl.col("flat_type") == option_flat)
-
-highest_price_per_town = highest_price_filtered.group_by("town").agg(
+highest_price_per_town = highest_price.group_by("town").agg(
     pl.max("resale_price").alias("max_resale_price")
 )
 
-highest_price_per_town = highest_price_filtered.join(
+highest_price_per_town = highest_price.join(
     highest_price_per_town,
     left_on=["town", "resale_price"],
     right_on=["town", "max_resale_price"],
     how="inner",
 )
 
-highest_price_per_town = highest_price_per_town.unique(subset=["town", "resale_price"])
+highest_price_per_town = highest_price_per_town.unique(
+    subset=["town", "resale_price"]
+).sort("town")
 highest_price_per_town = highest_price_per_town.sort("resale_price")
 
 ####################
@@ -67,14 +61,14 @@ sg_map = folium.Map(
     attr="OpenStreetMap",
 )
 for month, lat, lon, address, town, price, lease, level in zip(
-    highest_price_filtered["month"],
-    highest_price_filtered["latitude"],
-    highest_price_filtered["longitude"],
-    highest_price_filtered["address"],
-    highest_price_filtered["town"],
-    highest_price_filtered["resale_price"],
-    highest_price_filtered["remaining_lease_years"],
-    highest_price_filtered["storey_range"],
+    highest_price_per_town["month"],
+    highest_price_per_town["latitude"],
+    highest_price_per_town["longitude"],
+    highest_price_per_town["address"],
+    highest_price_per_town["town"],
+    highest_price_per_town["resale_price"],
+    highest_price_per_town["remaining_lease_years"],
+    highest_price_per_town["storey_range"],
 ):
     # pin colour
     if price > median_price:
@@ -119,7 +113,7 @@ ne = (
 
 sg_map.fit_bounds([sw, ne])
 
-st_data = st_folium(sg_map, width=1000)
+st_data = st_folium(sg_map, use_container_width=True)
 
 ##########################
 ### BAR CHART PLOTTING ###
@@ -139,7 +133,7 @@ fig.add_shape(
     x0=median_price,
     y0=-0.5,
     x1=median_price,
-    y1=len(highest_price_filtered) - 0.5,
+    y1=len(highest_price_per_town) - 0.5,
     line=dict(color="dark gray", width=2, dash="dash"),
 )
 
@@ -150,4 +144,4 @@ fig.update_layout(
     height=700,
 )
 
-st.plotly_chart(fig)
+st.plotly_chart(fig, height=700)
